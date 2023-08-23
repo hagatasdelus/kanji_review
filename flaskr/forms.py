08808cb789322a17934b31552ef5a9f1b07dc6c7
hagatasdelus <gsiyakav.c08nk@gmail.com>
2023-08-23 10:45:44 +0900

    update to search

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -35,7 +35,3 @@ class DeleteForm(FlaskForm):
 class SearchForm(FlaskForm):
     kanji = StringField('漢字: ', validators=[DataRequired()])
     submit = SubmitField('検索')
-
-    def validate_kanji(self, field):
-        if not Kanji.select_kanji_info_by_kanji(field.data):
-            raise ValidationError('その漢字は見つかりませんでした')
