38366fe14a46da84dd8988a41750be8e9af405ad
hagatasdelus <gsiyakav.c08nk@gmail.com>
2023-09-06 23:34:44 +0900

    update jsx file - simple...

--- flaskr/static/scripts/settings.jsx
+++ flaskr/static/scripts/settings.jsx
@@ -1,18 +1,65 @@
 import React, { useState } from 'react';
 
-const TypingGame = () => {
-  const [soundEnabled, setSoundEnabled] = useState(true);
+const SettingForm = () => {
+  const [circle, setCircle] = useState(false);
+  const [nextQTime, setNextQTime] = useState(5);
+  const [successSound, setSuccessSound] = useState(false);
 
-  const handleSoundToggle = () => {
-    setSoundEnabled(!soundEnabled);
-  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+        circle,
+        nextQTime,
+        successSound
+      };
+      const response = await fetch('/api/submit-form', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      const result = await response.json();
+
+  }
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <label>
-        <input type="checkbox" checked={soundEnabled} onChange={handleSoundToggle} />
-        SEを有効にする
+        サークル表示:
+        <input
+          name="circle"
+          type="checkbox"
+          checked={circle}
+          onChange={e => setCircle(e.target.checked)}
+        />
       </label>
-    </>
+      <br />
+      <label>
+        次の問題までの秒数(5~20s):
+        <input
+          name="nextQTime"
+          type="number"
+          min="5"
+          max="20"
+          value={nextQTime}
+          onChange={e => setNextQTime(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        SE:
+        <input
+          name="successSound"
+          type="checkbox"
+          checked={successSound}
+          onChange={e => setSuccessSound(e.target.checked)}
+        />
+      </label>
+      <br />
+      <input type="submit" value="設定" />
+    </form>
   );
-};
+}
+
+export default SettingForm;
